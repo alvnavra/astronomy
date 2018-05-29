c6ae75cf006006d7f20ed25c8728687c6d761971
harrydlr <harrydlr@gmail.com>
2018-05-29 20:06:56 +0200

    29mayo

--- our_fermi.py
+++ our_fermi.py
@@ -1,12 +1,22 @@
 import params
 import urllib.request
 
+def obtener_periodos(p_html):
+    return(float(p_html.replace('<td align=right>','')))
+
+def obtener_url(p_href):
+    ini_url = p_href.find('"')+1
+    tag_aux = p_href[ini_url:]
+    fin_url = tag_aux.find('"')
+    url = tag_aux[:fin_url]
+    return url
+   
 def tratar_tabla(p_tabla):
     tab = p_tabla
     tab = tab.replace('<table>','').replace('</table>','').replace("  ",'').replace('<tr>','').replace('<td>','').replace('<tr align=left>','')
     i = 0
     tr = 999
-    l_results = []
+    l_sources = []
     while tr > 0:
         tag = '</tr>'
         if i == 0:
@@ -17,6 +27,25 @@ def tratar_tabla(p_tabla):
             tr = tab.find(tag)+len(tag)
             tab_aux = tab[0:tr]
             l_lna = tab_aux.split('</td>')
+            ini_source = l_lna[0].find('">')+2
+            tag_surce = l_lna[0][ini_source:].replace("</a>","")
+            period1 = obtener_periodos(l_lna[1])
+            period2 = obtener_periodos(l_lna[2])
+            period3 = obtener_periodos(l_lna[3])
+            url_sw = obtener_url(l_lna[4])
+            url_maxi = obtener_url(l_lna[5])
+            url_simb = obtener_url(l_lna[6])
+            dict_source = {}
+            dict_source['source'] = tag_surce
+            dict_source['period1'] = period1
+            dict_source['period2'] = period2
+            dict_source['perido3'] = period3
+            dict_source['url_swift'] = url_sw
+            dict_source['url_maxi'] = url_maxi
+            dict_source['url_simbad'] = url_simb
+            l_sources.a(dict_source)
+    print(l_sources)
+
             
 
 
